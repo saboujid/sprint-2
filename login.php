8f7ba5147a098d5fcc3668930286e83e7fbed780
saboujid <saboujid.sa@gmail.com>
2022-03-30 12:38:34 +0100

    new login pages and changes

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
   <title> Corona Archive </title>
   <!-- <p style = "font-family:georgia,garamond,serif;font-size:70px;">
   <b> WELCOME TO THE UEFA CHAMPIONS LEAGUE INFO PAGE!</b> </p> -->
-  <link rel="stylesheet" href="styles.css"> 
+  <link rel = "stylesheet" href = "styles.css"> 
   <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
   <link
     rel="stylesheet"
@@ -22,19 +22,18 @@
 </head>
 <body>
       <div class="hero">
-        <div class="form-boxi">
+        <a href="index.php" class="back"><button class="back-btn" > Go back </button></a>
+        <div class="form-box2">
+            <div class="hp-text">
+                <h2>WHICH ONE ARE YOU?</h2>
+            </div>
             <div class="title">
-                <a href="login.php"><button type="button">Login</button></a>
-                <a href="register.php"><button type="button">Register</button></a>
+                <a href="visitor_login.php"><button type="button" class="Visitor">Visitor</button></a>
+                <a href="places_login.php"><button type="button" class="Place Owner">Place Owner</button></a>
+                <a href="agency_login.php"><button type="button" class="Agent">Agent</button></a>
+                <a href="hospital_login.php"><button type="button" class="Hospital">Hospital</button></a>
             </div>
         </div>
     </div>
-  <div class = "infoline">
-    <h1>The Best Web Service<br>for Corona Infections</h1>
-    <p>Get all the information you need about the places <br>
-    you have entered, displaying, evaluating and <br>
-    archiving the Corona infections in a particular location.</p>
-    <br><a href="" class="design">Learn More</a>
-  </div>
 </body>
 </html>
